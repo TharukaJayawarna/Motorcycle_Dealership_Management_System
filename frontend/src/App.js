c79ece894433f7eaf9180371a7c3f35f3caab4a1
it22319838 <it22319838@my.sliit.lk>
2024-05-11 20:58:22 +0530

    Suplier portal mawath ganna

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -78,18 +78,18 @@ import AdditionalPayment from "./Components/AdditionalPayment";
 import UpdateEmployeeSalaryForm from "./Components/UpdateEmployeeSalary";
 import FinancialReport from "./Components/FinancialReport";
 
-import Home from './Components/pages/Home12';
-
+import Home12 from './Components/pages/Home12';
 import Profile12 from './Components/pages/Profile12';
 import Dashboard from './Components/pages/Dashboard';
 import Suppliers from './Components/pages/Suppliers';
 import Notification from './Components/pages/Notification';
-import Report from './Components/pages/Report';
+import Report12 from './Components/pages/Report';
 
 import Avalibility from './Components/pages/Avalibility/Avalibilitylist';  
 import Register12 from './Components/pages/Register12';
 import Login12 from './Components/pages/Login12';
 import Logout12 from './Components/pages/Logout12';
+import Home1234 from "./Components/pages/Home12";
 
 function App() {
   const ProtectedRoute = ({ children }) => {
@@ -224,14 +224,14 @@ function App() {
         </Routes>
 
         <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/123" element={<Home1234 />} />
       <Route path="/register12" element={<Register12 />} />
       <Route path="/login12" element={<Login12 />} />
       <Route path="/profile12" element={<Profile12 />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/suppliers" element={<Suppliers />} />
       <Route path="/notification" element={<Notification />} />
-       <Route path="/report" element={<Report />} />
+       <Route path="/report" element={<Report12 />} />
       <Route path="/logout12" element={<Logout12 />} />
       <Route path="/availability" element={<Avalibility />} /> 
 
