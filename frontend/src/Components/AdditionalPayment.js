37ab9d74238d22ce4d80b641c204415afc77afbd
AnjaliMoragaspitiya <159754686+AnjaliMoragaspitiya@users.noreply.github.com>
2024-05-11 18:54:44 +0530

    financial management

--- frontend/src/Components/AdditionalPayment.js
+++ frontend/src/Components/AdditionalPayment.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import './colours.css';
 
 const AdditionalPayment = () => {
     const [additionalPayments, setAdditionalPayments] = useState([]);
--- frontend/src/Components/ManagerDashboard/ManagerDashboard.js
+++ frontend/src/Components/ManagerDashboard/ManagerDashboard.js
@@ -3,6 +3,7 @@ import Nav from "../Nav/Nav";
 import NavigationBar from "../ManagerNavigation/NavigationBar";
 
 const ManagerDashboard = () => {
+  //const [totalUsers, setTotalUsers] = useState(0);
   return (
     <div>
       <Nav /> ManagerDashboard
